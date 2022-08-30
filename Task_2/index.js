d9543f6e610143a67bbe2e048d0609c900343597
kusharechaitu <112420578+kusharechaitu@users.noreply.github.com>
2022-08-31 00:02:04 +0530

    Update index.js

--- Task_2/index.js
+++ Task_2/index.js
@@ -1,26 +1,40 @@
-let screen = document.getElementById('screen');
-buttons = document.querySelectorAll('button');
-let screenValue = '';
-for (item of buttons) {
-    item.addEventListener('click', (e) => {
-        buttonText = e.target.innerText;
-        console.log('Button text is ', buttonText);
-        if (buttonText == 'X') {
-            buttonText = '*';
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
-        else if (buttonText == 'C') {
-            screenValue = "";
-            screen.value = screenValue;
-        }
-        else if (buttonText == '=') {
-            screen.value = eval(screenValue);
-        }
-        else {
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
+.container{
+    text-align: center;
+    margin-top:24px
+}
 
-    })
+table{
+    margin: auto;
+}
+
+/* creating input taking display */
+input{
+    border-radius: 21px;
+    border: 5px solid rgb(0, 0, 0);
+    font-size:34px;
+    height: 65px;
+    width: 400px;
+}
+/* creating butthon colour and border */
+button{
+    border-radius: 20px;
+    font-size: 20px;
+    background: hsl(176, 33%, 52%);
+    width: 80px;
+    height: 80px;
+    margin: 6px;
+}
+ /* creating the colour and button for calculator */
+.calculator{ 
+    border: 4px solid hsl(0, 0%, 2%);
+    background-color: hwb(289 100% 0%);
+    padding: 12px;
+    border-radius: 30px;
+    display: inline-block;
+    
+}
+
+h1{
+    font-size: 22px;
+    font-family: cursive;
 }
